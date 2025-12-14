5378be5591ae6d6431073072b559382559dba52b
muaziqbal <sheikhmuaz111@gmail.com>
2025-12-14 16:50:25 +0500

    resolved path issue

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import Search from '@/app/ui/search';
+import { lusitana } from '@/ui/ui/fonts';
+import Search from '@/ui/ui/search';
 import {
   CustomersTableType,
   FormattedCustomersTable,
-} from '@/app/lib/definitions';
+} from '@/ui/lib/definitions';
 
 export default async function CustomersTable({
   customers,
